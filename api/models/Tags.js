cfcb9aa90cc6b101e7ea8df6ea1741b857ad44a5
Nishchit14 <dnishchit@gmail.com>
2018-05-12 17:46:58 +0530

    some changes in model

---
+++ api/models/Tags.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const Model = require('trails/model')
+
+/**
+ * @module Tags
+ * @description tags
+ */
+module.exports = class Tags extends Model {
+
+    static config(app, Sequelize) {
+        return {
+            options: {
+
+                classMethods: {
+                    associate: (models) => {
+                        models.Tags.belongsTo(models.User, {
+                            targetKey: 'id',
+                            foreignKey: 'userId',
+                            onDelete: 'NO ACTION'
+                        })
+                    }
+                }
+            }
+        }
+    }
+
+    static schema(app, Sequelize) {
+
+        let {INTEGER, DATE, literal, STRING, TEXT, BOOLEAN} = Sequelize;
+
+        return {
+            userId: {type: INTEGER, allowNull: false},
+            title: {type: STRING, allowNull: false},
+            createdAt: {
+                type: DATE,
+                field: 'createdAt',
+                defaultValue: literal('NOW()'),
+            },
+            updatedAt: {
+                type: DATE,
+                field: 'updatedAt',
+                defaultValue: literal('NOW()'),
+            },
+        }
+    }
+}
---
+++ test/unit/models/Tags.test.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const assert = require('assert')
+
+describe('Tags Model', () => {
+  it('should exist', () => {
+    assert(global.app.models.Tags)
+  })
+})
