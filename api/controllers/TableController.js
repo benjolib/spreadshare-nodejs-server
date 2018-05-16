98bcaadb41df72c3df9a5ceaa7604077154f58d2
Nishchit14 <dnishchit@gmail.com>
2018-05-16 16:14:07 +0530

    Table api created

--- api/controllers/TableController.js
+++ api/controllers/TableController.js
@@ -1,282 +1,265 @@
-'use strict'
+"use strict";
 
-const Controller = require('trails/controller')
+const Controller = require("trails/controller");
 
 /**
  * @module TableController
  * @description table.
  */
 module.exports = class TableController extends Controller {
-    /**
-     * create table
-     * @param req
-     * @param res
-     * @returns {Promise<*>}
-     */
-    async create(req, res) {
+  /**
+   * create table
+   * @param req
+   * @param res
+   * @returns {Promise<*>}
+   */
+  async create(req, res) {
+    let model = req.body;
+    let { TableService } = this.app.services;
+    let user = req.user;
+    try {
+      let data = {
+        owner: user.id,
+        title: model.title,
+        tags: model.tags,
+        tagline: model.tagline,
+        image: model.image,
+        description: model.description,
+        isThumbnail: model.isThumbnail,
+        curator: model.curator,
+        isPublished: model.isPublished
+      };
 
-        let model = req.body
-        let {TableService} = this.app.services
-        let user = req.user
-        try {
-
-            let data = {
-                owner: user.id,
-                title: model.title,
-                tags: model.tags,
-                tagline: model.tagline,
-                image: model.image,
-                description: model.description,
-                isThumbnail: model.isThumbnail,
-                curator: model.curator,
-                isPublished: model.isPublished
-            }
-
-            let table = await TableService.create(data)
-            return res.json({
-                flag: true,
-                data: table,
-                message: "Success",
-                code: 200
-            });
-        }
-        catch (e) {
-            return res.json({
-                flag: false,
-                data: e,
-                message: e.message,
-                code: 500
-            });
-        }
+      let table = await TableService.create(data);
+      return res.json({
+        flag: true,
+        data: table,
+        message: "Success",
+        code: 200
+      });
+    } catch (e) {
+      return res.json({
+        flag: false,
+        data: e,
+        message: e.message,
+        code: 500
+      });
     }
+  }
 
-    /**
-     * add column
-     * @param req
-     * @param res
-     * @returns {Promise<*>}
-     */
-    async addColumn(req, res) {
-        let model = req.body
-        let {TableService} = this.app.services
-        let user = req.user
-        let data = {
-            tableId: model.tableId,
-            userId: user.id,
-            title: model.title,
-            position: model.position,
-            width: model.width
-        }
+  /**
+   * add column
+   * @param req
+   * @param res
+   * @returns {Promise<*>}
+   */
+  async addColumn(req, res) {
+    let model = req.body;
+    let { TableService } = this.app.services;
+    let user = req.user;
+    let data = {
+      tableId: model.tableId,
+      userId: user.id,
+      title: model.title,
+      position: model.position,
+      width: model.width
+    };
 
-        try {
-            let column = await TableService.addColumn(data)
-            return res.json({
-                flag: true,
-                data: column,
-                message: "Success",
-                code: 200
-            });
-        }
-        catch (e) {
-            return res.json({
-                flag: false,
-                data: e,
-                message: e.message,
-                code: 500
-            });
-        }
+    try {
+      let column = await TableService.addColumn(data);
+      return res.json({
+        flag: true,
+        data: column,
+        message: "Success",
+        code: 200
+      });
+    } catch (e) {
+      return res.json({
+        flag: false,
+        data: e,
+        message: e.message,
+        code: 500
+      });
     }
+  }
 
-    /**
-     * update column
-     * @param req
-     * @param res
-     * @returns {Promise<*>}
-     */
-    async updateColumn(req, res) {
+  /**
+   * update column
+   * @param req
+   * @param res
+   * @returns {Promise<*>}
+   */
+  async updateColumn(req, res) {
+    let params = req.params;
+    let model = req.body;
+    let id = parseInt(params.id);
+    let { TableService } = this.app.services;
 
-        let params = req.params
-        let model = req.body
-        let id = parseInt(params.id);
-        let {TableService} = this.app.services
-
-        try {
-            let column = await TableService.updateColumn(model, id)
-            return res.json({
-                flag: true,
-                data: column,
-                message: "Success",
-                code: 200
-            });
-        }
-        catch (e) {
-            return res.json({
-                flag: false,
-                data: e,
-                message: e.message,
-                code: 500
-            });
-        }
+    try {
+      let column = await TableService.updateColumn(model, id);
+      return res.json({
+        flag: true,
+        data: column,
+        message: "Success",
+        code: 200
+      });
+    } catch (e) {
+      return res.json({
+        flag: false,
+        data: e,
+        message: e.message,
+        code: 500
+      });
     }
+  }
 
-    /**
-     * remove column
-     * @param req
-     * @param res
-     * @returns {Promise<*>}
-     */
-    async removeColumn(req, res) {
-
-        let params = req.params
-        let id = parseInt(params.id);
-        let {TableService} = this.app.services
-        try {
-            let column = await TableService.removeColumn(id)
-            return res.json({
-                flag: true,
-                data: column,
-                message: "Success",
-                code: 200
-            });
-        }
-        catch (e) {
-            return res.json({
-                flag: false,
-                data: e,
-                message: e.message,
-                code: 500
-            });
-        }
+  /**
+   * remove column
+   * @param req
+   * @param res
+   * @returns {Promise<*>}
+   */
+  async removeColumn(req, res) {
+    let params = req.params;
+    let id = parseInt(params.id);
+    let { TableService } = this.app.services;
+    try {
+      let column = await TableService.removeColumn(id);
+      return res.json({
+        flag: true,
+        data: column,
+        message: "Success",
+        code: 200
+      });
+    } catch (e) {
+      return res.json({
+        flag: false,
+        data: e,
+        message: e.message,
+        code: 500
+      });
     }
+  }
 
-    /**
-     * update table
-     * @param req
-     * @param res
-     * @returns {Promise<*>}
-     */
-    async update(req, res) {
+  /**
+   * update table
+   * @param req
+   * @param res
+   * @returns {Promise<*>}
+   */
+  async update(req, res) {
+    let params = req.params;
+    let model = req.body;
+    let id = parseInt(params.id);
+    let { TableService } = this.app.services;
 
-        let params = req.params
-        let model = req.body
-        let id = parseInt(params.id);
-        let {TableService} = this.app.services
-
-        try {
-            let table = await TableService.update(model, id)
-            //todo Update column pending
-            return res.json({
-                flag: true,
-                data: table,
-                message: "Success",
-                code: 200
-            });
-        }
-        catch (e) {
-            return res.json({
-                flag: false,
-                data: e,
-                message: e.message,
-                code: 500
-            });
-        }
+    try {
+      let table = await TableService.update(model, id);
+      //todo Update column pending
+      return res.json({
+        flag: true,
+        data: table,
+        message: "Success",
+        code: 200
+      });
+    } catch (e) {
+      return res.json({
+        flag: false,
+        data: e,
+        message: e.message,
+        code: 500
+      });
     }
+  }
 
-    /**
-     * table published
-     * @param req
-     * @param res
-     * @returns {Promise<*>}
-     */
-    async publish(req, res) {
-
-        let params = req.params
-        let model = req.body
-        let id = parseInt(params.id);
-        let {TableService} = this.app.services
-        let time = new Date().getTime()
-        let data = {
-            isPublished: model.isPublished,
-            publishedAt: time
-        }
-        try {
-            let table = await TableService.update(data, id)
-            return res.json({
-                flag: true,
-                data: table,
-                message: "Success",
-                code: 200
-            });
-        }
-        catch (e) {
-            return res.json({
-                flag: false,
-                data: e,
-                message: e.message,
-                code: 500
-            });
-        }
+  /**
+   * table published
+   * @param req
+   * @param res
+   * @returns {Promise<*>}
+   */
+  async publish(req, res) {
+    let params = req.params;
+    let model = req.body;
+    let id = parseInt(params.id);
+    let { TableService } = this.app.services;
+    let time = new Date().getTime();
+    let data = {
+      isPublished: model.isPublished,
+      publishedAt: time
+    };
+    try {
+      let table = await TableService.update(data, id);
+      return res.json({
+        flag: true,
+        data: table,
+        message: "Success",
+        code: 200
+      });
+    } catch (e) {
+      return res.json({
+        flag: false,
+        data: e,
+        message: e.message,
+        code: 500
+      });
     }
+  }
 
-    /**
-     * get table details
-     * @param req
-     * @param res
-     * @returns {Promise<*>}
-     */
-    async tableDetail(req, res) {
+  /**
+   * get table details
+   * @param req
+   * @param res
+   * @returns {Promise<*>}
+   */
+  async tableDetail(req, res) {
+    let { TableService } = this.app.services;
+    let params = req.params;
+    let id = parseInt(params.id);
 
-        let {TableService} = this.app.services
-        let params = req.params
-        let id = parseInt(params.id);
-
-        try {
-            let table = await TableService.find(id)
-            return res.json({
-                flag: true,
-                data: table,
-                message: "Success",
-                code: 200
-            });
-        }
-        catch (e) {
-            return res.json({
-                flag: false,
-                data: e,
-                message: e.message,
-                code: 500
-            });
-        }
+    try {
+      let table = await TableService.find(id);
+      return res.json({
+        flag: true,
+        data: table,
+        message: "Success",
+        code: 200
+      });
+    } catch (e) {
+      return res.json({
+        flag: false,
+        data: e,
+        message: e.message,
+        code: 500
+      });
     }
+  }
 
-    /**
-     * remove table
-     * @param req
-     * @param res
-     * @returns {Promise<*>}
-     */
-    async remove(req,res){
-        let params = req.params
-        let id = parseInt(params.id);
-        let {TableService} = this.app.services
-        try {
-            let table = await TableService.remove(id)
-            return res.json({
-                flag: true,
-                data: table,
-                message: "Success",
-                code: 200
-            });
-        }
-        catch (e) {
-            return res.json({
-                flag: false,
-                data: e,
-                message: e.message,
-                code: 500
-            });
-        }
+  /**
+   * remove table
+   * @param req
+   * @param res
+   * @returns {Promise<*>}
+   */
+  async remove(req, res) {
+    let params = req.params;
+    let id = parseInt(params.id);
+    let { TableService } = this.app.services;
+    try {
+      let table = await TableService.remove(id);
+      return res.json({
+        flag: true,
+        data: table,
+        message: "Success",
+        code: 200
+      });
+    } catch (e) {
+      return res.json({
+        flag: false,
+        data: e,
+        message: e.message,
+        code: 500
+      });
     }
-}
-
-
+  }
+};
--- api/services/TableService.js
+++ api/services/TableService.js
@@ -1,127 +1,104 @@
-'use strict'
+"use strict";
 
-const Service = require('trails/service')
-const _ = require('lodash')
+const Service = require("trails/service");
+const _ = require("lodash");
 /**
  * @module TableService
  * @description table
  */
 module.exports = class TableService extends Service {
+  /**
+   * create table
+   * @param fields
+   * @returns {*|PromiseLike<T>|Promise<T>}
+   */
+  create(fields) {
+    let { Table } = this.app.orm;
 
-    /**
-     * create table
-     * @param fields
-     * @returns {*|PromiseLike<T>|Promise<T>}
-     */
-    create(fields) {
+    return Table.create(fields).then(data => {
+      if (_.isEmpty(data)) throw new Error(`Table not Created!.`);
+      return data.toJSON();
+    });
+  }
 
-        let {Table} = this.app.orm
+  /**
+   * add column
+   * @param fields
+   * @returns {*|PromiseLike<T>|Promise<T>}
+   */
+  addColumn(fields) {
+    let { TableColumn } = this.app.orm;
 
-        return Table
-            .create(fields)
-            .then(data => {
-                console.log(data)
-                return data.toJSON()
-            })
-    }
+    return TableColumn.create(fields).then(data => {
+      if (_.isEmpty(data)) throw new Error(`Table Column not Created!.`);
+      return data.toJSON();
+    });
+  }
 
-    /**
-     * add column
-     * @param fields
-     * @returns {*|PromiseLike<T>|Promise<T>}
-     */
-    addColumn(fields) {
+  /**
+   * update column
+   * @param fields
+   * @param id
+   * @returns {*|PromiseLike<T>|Promise<T>}
+   */
+  updateColumn(fields, id) {
+    let { TableColumn } = this.app.orm;
 
-        let {TableColumn} = this.app.orm
+    return TableColumn.update(fields, { where: { id } }).then(data => {
+      return data;
+    });
+  }
 
-        return TableColumn
-            .create(fields)
-            .then(data => {
-                console.log(data)
-                return data.toJSON()
-            })
-    }
+  /**
+   * delete column
+   * @param id
+   * @returns {*|PromiseLike<T>|Promise<T>}
+   */
+  removeColumn(id) {
+    let { TableColumn } = this.app.orm;
 
-    /**
-     * update column
-     * @param fields
-     * @param id
-     * @returns {*|PromiseLike<T>|Promise<T>}
-     */
-    updateColumn(fields, id) {
+    return TableColumn.destroy({ where: { id } }).then(data => {
+      return data;
+    });
+  }
 
-        let {TableColumn} = this.app.orm
+  /**
+   * update table
+   * @param fields
+   * @param id
+   * @returns {*|PromiseLike<T>|Promise<T>}
+   */
+  update(fields, id) {
+    let { Table } = this.app.orm;
 
-        return TableColumn
-            .update(fields, {where: {id}})
-            .then(data => {
-                return data
-            })
-    }
+    return Table.update(fields, { where: { id } }).then(data => {
+      return data;
+    });
+  }
 
-    /**
-     * delete column
-     * @param id
-     * @returns {*|PromiseLike<T>|Promise<T>}
-     */
-    removeColumn(id) {
+  /**
+   * get table details
+   * @param id
+   * @returns {*|PromiseLike<T>|Promise<T>}
+   */
+  find(id) {
+    let { Table } = this.app.orm;
+    return Table.find({ where: id }).then(data => {
+      if (_.isEmpty(data)) throw new Error(`Table not found!.`);
+      return data.toJSON();
+    });
+  }
 
-        let {TableColumn} = this.app.orm
+  /**
+   * remove table
+   * @param id
+   * @returns {*|PromiseLike<T>|Promise<T>}
+   */
+  remove(id) {
+    let { Table } = this.app.orm;
 
-        return TableColumn
-            .destroy({where: {id}})
-            .then(data => {
-                return data
-            })
-    }
-
-    /**
-     * update table
-     * @param fields
-     * @param id
-     * @returns {*|PromiseLike<T>|Promise<T>}
-     */
-    update(fields, id) {
-
-        let {Table} = this.app.orm
-
-        return Table
-            .update(fields, {where: {id}})
-            .then(data => {
-                return data
-            })
-    }
-
-    /**
-     * get table details
-     * @param id
-     * @returns {*|PromiseLike<T>|Promise<T>}
-     */
-    find(id) {
-
-        let {Table} = this.app.orm
-        return Table
-            .find({where: id})
-            .then(data => {
-                return data.toJSON()
-            })
-    }
-
-    /**
-     * remove table
-     * @param id
-     * @returns {*|PromiseLike<T>|Promise<T>}
-     */
-    remove(id) {
-
-        let {Table} = this.app.orm
-
-        return Table
-            .destroy({where: {id}})
-            .then(data => {
-                return data
-            })
-    }
-
-}
-
+    return Table.destroy({ where: { id } }).then(data => {
+      return data;
+    });
+  }
+};
