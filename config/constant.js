3b14ef5f115c6a66f6bb856938567fcf5fa96411
Nishchit14 <dnishchit@gmail.com>
2018-05-17 12:22:14 +0530

    table constant created

--- config/constant.js
+++ config/constant.js
@@ -2,23 +2,30 @@
  * Constants
  */
 
-'use strict'
+"use strict";
 
 module.exports = {
-
-    user: {
-        type: {
-            FOLLOWER:'F',
-            COLLABORATIONS : 'COL',
-            SUBMISSIONS: 'S',
-            COMMENTS: 'COM',
-            NEW_LISTS: 'N',
-            SUMMARY: 'S',
-            UPDATES: 'U',
-        },
-        status:{
-            ACTIVE : 'A',
-            DEACTIVE: 'D',
-        }
+  user: {
+    type: {
+      FOLLOWER: "F",
+      COLLABORATIONS: "COL",
+      SUBMISSIONS: "S",
+      COMMENTS: "COM",
+      NEW_LISTS: "N",
+      SUMMARY: "S",
+      UPDATES: "U"
+    },
+    status: {
+      ACTIVE: "A",
+      DEACTIVE: "D"
     }
-}
+  },
+  tables: {
+    TABLE: "table",
+    TABLE_VIEW: "tableview",
+    TABLE_ROW: "tablerow",
+    USER: "user",
+    TABLE_SUBSCRIPTION: "tablesubscription",
+    TABLE_INFO: "tableinfo"
+  }
+};
