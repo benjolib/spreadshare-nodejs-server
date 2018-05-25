184f223bb3f731cfb55b4f301fc35365e9db2432
Nishchit14 <dnishchit@gmail.com>
2018-05-25 14:45:16 +0530

    create trigger for comment vote

---
+++ migrations/20180525084532-add-trigger-comment-votedb-migrate.js
@@ -0,0 +1,47 @@
+"use strict";
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  let schema = db.schema;
+  let sql = `
+    CREATE OR REPLACE FUNCTION ${schema}.table_comment_inc() 
+    RETURNS trigger
+    LANGUAGE plpgsql
+    AS $function$
+    BEGIN
+    EXECUTE 'UPDATE ' || TG_TABLE_SCHEMA || '.tablecomment SET "votesCount" = "votesCount"+1 WHERE "id" = $1."itemId"' USING NEW;
+    RETURN NULL;
+    END
+    $function$;
+    
+    CREATE TRIGGER incr_table_comment AFTER INSERT ON ${schema}.vote
+    FOR EACH ROW EXECUTE procedure ${schema}.table_comment_inc();`;
+
+  return db.runSql(sql);
+};
+
+exports.down = function(db) {
+  let schema = db.schema;
+
+  let sql = `DROP TRIGGER IF EXISTS incr_table_spreads ON ${schema}.vote;
+             DROP FUNCTION IF EXISTS ${schema}.table_spread_incr();`;
+
+  return db.runSql(sql);
+};
+
+exports._meta = {
+  version: 1
+};
